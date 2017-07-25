90667f596e6ce30a5be59645537f82d7b20a69b1
Roberto Lama <roberlama@gmail.com>
2017-07-25 17:06:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 ID = '00:16:53:00:F5:A9'
 
+import sys
+
 # Just the imports for using nxt
 import nxt.locator
 from nxt.motor import *
@@ -9,9 +11,15 @@ from nxt.motor import Motor, PORT_A, PORT_B, PORT_C
 from nxt.sensor import Light, Sound, Touch, Ultrasonic
 from nxt.sensor import PORT_1, PORT_2, PORT_3, PORT_4
 
-# Locate the brick
-b = nxt.locator.find_one_brick()
-print "connected"
+print "Finding a brick..."
+
+try:
+    # Locate the brick
+    b = nxt.locator.find_one_brick()
+    print "Brick connected"
+except:
+    print "Sorry, we couldn't find a nxt brick..."
+    sys.exit()
 
 # Here you can change the frequencies to change the notes
 FREQ_C = 523
@@ -32,34 +40,38 @@ if b:
     # Main while
     while True:
         # When you press the button
-        if Touch(b, PORT_4).get_sample():
-            # Obtain ultrasonic sample (distance)
-            note = Ultrasonic(b, PORT_1).get_sample()
-            # Obtain the position of the motor for adjust the note's lenght
-            time = Motor(b, PORT_A).get_tacho().block_tacho_count
-            # Associate the position of the motor with the lenght of the note
-            if time > 0 and time < 20:
-                time = 100
-            elif time > 20 and time < 40:
-                time = 500
-            else:
-                time = 1000
-            # Associate the fret position with a note
-            if note < 9:
-                b.play_tone(FREQ_C, 100+time)
-                print "C", note, time
-            elif note >= 9 and note < 13:
-                b.play_tone(FREQ_D, 100+time)
-                print "D",note,time
-            elif note >= 13 and note < 17:
-                b.play_tone(FREQ_E, 100+time)
-                print "E",note,time
-            elif note >= 17 and note < 20:
-                b.play_tone(FREQ_F, 100+time)
-                print "F",note,time
-            else:
-                b.play_tone(FREQ_G, 100+time)
-                print "G",note,time
+        try:
+            if Touch(b, PORT_4).get_sample():
+                # Obtain ultrasonic sample (distance)
+                note = Ultrasonic(b, PORT_1).get_sample()
+                # Obtain the position of the motor for adjust the note's lenght
+                time = Motor(b, PORT_A).get_tacho().block_tacho_count
+                # Associate the position of the motor with the lenght of the note
+                if time > 0 and time < 20:
+                    time = 100
+                elif time > 20 and time < 40:
+                    time = 500
+                else:
+                    time = 1000
+                # Associate the fret position with a note
+                if note < 9:
+                    b.play_tone(FREQ_C, 100+time)
+                    print "C", note, time
+                elif note >= 9 and note < 13:
+                    b.play_tone(FREQ_D, 100+time)
+                    print "D",note,time
+                elif note >= 13 and note < 17:
+                    b.play_tone(FREQ_E, 100+time)
+                    print "E",note,time
+                elif note >= 17 and note < 20:
+                    b.play_tone(FREQ_F, 100+time)
+                    print "F",note,time
+                else:
+                    b.play_tone(FREQ_G, 100+time)
+                    print "G",note,time
+        except:
+            print "Brick disconnected"
+            sys.exit()
 
 else:
    print 'No NXT bricks found'
